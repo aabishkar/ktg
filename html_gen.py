9c43d9af4e7142a8a0739a29b013282933d3a486
ganymede_mac <abishkar@gmail.com>
2021-12-11 12:47:22 +0545

    py added mac

---
+++ html_gen.py
@@ -0,0 +1,23 @@
+html_str = """
+<table border=1>
+     <tr>
+       <th>Number</th>
+       <th>Square</th>
+     </tr>
+     <indent>
+     <% for i in range(10): %>
+       <tr>
+         <td><%= i %></td>
+         <td><%= i**2 %></td>
+       </tr>
+     </indent>
+</table>
+"""
+
+html_str += """
+  <b>aabishkar</b>
+"""
+
+Html_file= open("filename.html","w")
+Html_file.write(html_str)
+Html_file.close()
